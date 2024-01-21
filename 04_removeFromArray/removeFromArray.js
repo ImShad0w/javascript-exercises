d1deac3432f0afa43642fee720456c312fd33e19
ImShad0w <kirill19042006@gmail.com>
2024-01-21 14:50:12 +0100

    Added function and enabled test, still in development

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, value) {
+  //Gets the index of the specified value
+  const index = array.indexOf(value);
+  //Only splices the array when the value is found
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+  return array;
 };
-
+let result = removeFromArray([1, 2, 3, 4, 5], 3, 4);
+console.log(result);
 // Do not edit below this line
 module.exports = removeFromArray;
