9b70d003d76d3f940e7ad12a641ecb1fdaedd9b6
ImShad0w <kirill19042006@gmail.com>
2024-01-24 09:17:14 +0100

    Created function that deletes multiple items in an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,14 @@
-const removeFromArray = function (array, value) {
-  //Gets the index of the specified value
-  const index = array.indexOf(value);
-  //Only splices the array when the value is found
-  if (index > -1) {
-    array.splice(index, 1);
-  }
+const removeFromArray = function (array, ...values) {
+  //Loops through all of the items in the array
+  values.forEach((value) => {
+    //Sets the index of the value as the constant to find the value in the array
+    const index = array.indexOf(value);
+    if (index !== -1) {
+      //Once it finds the matching value it will splice it
+      array.splice(index, 1);
+    }
+  });
+  //At the end it returns the array with the result
   return array;
 };
 let result = removeFromArray([1, 2, 3, 4, 5], 3, 4);
