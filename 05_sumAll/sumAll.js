33a64abdea99c36225d8360d0414b5f78b0173b3
ImShad0w <kirill19042006@gmail.com>
2024-01-28 16:28:58 +0100

    Added function and enabled test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  let sumResult = 0;
+  //If the first number is bigger switch places
+  if (startNum > endNum) {
+    const swap = startNum;
+    startNum = endNum;
+    endNum = swap;
+  }
 
+  //Sums all the numbers between the start and end numbers
+  for (let num = startNum; num <= endNum; num++) {
+    sumResult += num;
+  }
+  return sumResult;
 };
 
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
