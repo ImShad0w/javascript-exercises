dcf4942b12d01d7132d5c447a2a41fe1cec34bf2
ImShad0w <kirill19042006@gmail.com>
2024-02-08 17:19:27 +0100

    Added the functions now works perfectly

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+  //If the year is divisible by 4
+if (year % 4 === 0){
+    //If the year is also divisible by 100 but not 400
+    if (year % 100 === 0 && year % 400 !== 0){
+      return false;
+    }else{
+      return true;
+    }
+    //If its not divisible by 4
+  }else{
+    return false;
+  }
+}
 // Do not edit below this line
 module.exports = leapYears;
