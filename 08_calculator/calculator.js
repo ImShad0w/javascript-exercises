c6b9fb65b397f3478dac135d1002bcea12347e24
ImShad0w <kirill19042006@gmail.com>
2024-05-21 22:53:23 +0200

    Added functions, works now

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let sum = a + b
+
+  return sum
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(a, b) {
+  let result = a - b;
+  return result
 
 };
 
-const power = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const multiply = function(arr) {
+  let mult = 1;
+  for (let i = 0; i < arr.length; i++) {
+    mult *= arr[i];
+  }
+  return mult
+};
+
+const power = function(num, power) {
+  let pow = num ** power
+  return pow;
+};
+
+const factorial = function(a) {
+  let result = a
+  if (a === 0 || a === 1) {
+    return 1;
+  }
+  while (a > 1) {
+    a--;
+    result *= a;
+  }
+  return result;
 };
 
 // Do not edit below this line
